549a1b84c5e4ebced8a3ab6db0e43435213b3c8b
aaid <zirekaaid@gmail.com>
2024-08-08 20:17:31 +0100

    added

---
+++ every_js/Random_number_generator.js
@@ -0,0 +1,27 @@
+// RANDOM NUMBER GENRATOR
+
+//const min = 50;
+//const max = 100;
+
+//let randomNum = Math.floor(Math.random() * (max-min)) + min;
+//console.log(randomNum);
+
+
+const myBtuton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max =10;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myBtuton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}

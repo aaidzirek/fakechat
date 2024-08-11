51544ac1eac6bac9f0d15de5b73f3ed43a18758b
aaid <zirekaaid@gmail.com>
2024-08-11 17:40:24 +0100

    added

---
+++ every_js/Checked_property.js
@@ -0,0 +1,34 @@
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subReslut = document.getElementById("subReslut");
+const paymentReslut = document.getElementById("paymentReslut");
+
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+        subReslut.textContent = `you are sybscribed!?`;
+    }
+    else{
+        subReslut.textContent = `you are NOT sybscribed!?`;
+    }
+
+    if(visaBtn.checked){
+        paymentReslut.textContent = `you are pauing with visa✔️`;
+    }
+    else{
+        paymentReslut.textContent = `you are NOT pauing with visa✖️`;
+    }
+    if(masterCardBtn.checked){
+        paymentReslut.textContent = `you are pauing with MasterCard✔️`;
+    }
+    if(payPalBtn.checked){
+        paymentReslut.textContent = `you are pauing with payPal✔️`;
+    }
+    else{
+        paymentReslut.textContent = `you must selcet a payment type`;
+    }
+
+}

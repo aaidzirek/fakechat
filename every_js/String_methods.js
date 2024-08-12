619dca8868ffe313710e6ba40f70c895c6217e14
aaid <zirekaaid@gmail.com>
2024-08-12 18:53:08 +0100

    added

---
+++ every_js/String_methods.js
@@ -0,0 +1,23 @@
+// .charAt
+// console.log(userName.charAt(0));
+// console.log(userName.indexOf("o"));
+// console.log(userName.length);
+// .toUpperCase   .toLowerCase   .repeat   .startWith  .endsWith
+
+//let userName = "Brocode";
+
+//let result = userName.includes(" ");
+
+//if(result){
+//    console.log("your username can't include ' '");
+//}
+//else{
+//    console.log(userName);
+// }
+
+
+
+let phoneNumber = "066-444-0628"
+phoneNumber = phoneNumber.padStart(15, "0");
+
+console.log(phoneNumber);
---
+++ every_js/String_slicing.js
@@ -0,0 +1,24 @@
+
+/*
+const fullName = "Aaid abdalah"
+
+// let firstName = fullName.slice(0, 4);
+// let lastName = fullName.slice(5, 12);
+
+// let firstchar = fullName.slice(0, 1);
+//let lastchar = firstName.slice(-1);
+
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+console.log(firstName);
+console.log(lastName);
+// console.log(lastName);
+ */
+
+const email = "zirekaaid@gmail.com";
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1); 
+
+console.log(username);
+console.log(extension);

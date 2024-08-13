50d41660fc8c26f91cd177fc876a2bf841904c78
aaid <zirekaaid@gmail.com>
2024-08-13 19:54:21 +0100

    added

---
+++ every_js/For_loops.js
@@ -0,0 +1,17 @@
+//  while loop = repeat some code a LIMITED amount of times
+/*
+for(let i = 10; i > 0; i--){
+    console.log(i);
+}
+console.log("HAOOY NEW YEAR")*/
+  
+for(let i = -1; i <= 20; i++){
+
+    if(i == 13){
+        // break;
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}

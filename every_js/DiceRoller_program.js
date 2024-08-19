7c283d194d2a35a938fb6ef73dca33eb946f502d
aaid <zirekaaid@gmail.com>
2024-08-19 13:07:40 +0100

    Hangman

--- every_js/DiceRoller_program.js
+++ every_js/DiceRoller_program.js
@@ -1,21 +1,25 @@
 // DIICE ROLLER PROGRAM
 
 function rollDice(){
+    
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];      
+    const images = [];
 
-        const numOfDice = document.getElementById("numOfDice").values;
-        const diceResult = document.getElementById("diceResult");
-        const diceImages = document.getElementById("diceImages");
-        const values = [];
-        const images = [];
-
-        for(let i = 0; i < numOfDice; i++){
+    for(let i = 0; i < numOfDice; i++){
+        if(numOfDice > 6){
+            document.getElementById("error").innerText = "please select a number between 1-6";
+        }else{
             const value = Math.floor(Math.random() * 6) + 1;
-         // console.log(value);
+            // console.log(value);
             values.push(value);
-            images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
+            images.push(`<img src="../dice_images/${value}.png" alt="Dice ${value}">`);
+            document.getElementById("error").innerText = "";
         }
-         // console.log(values);
-         diceResult.textContent = `dice: ${values.join(', ')}`;
-         diceImages.innerHTML = images.join('');
-
+      
+    }
+    numOfDice > 6 ? diceResult.textContent = "" : diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
 }
--- every_js/Temperature_conversion_program.js
+++ every_js/Temperature_conversion_program.js
@@ -6,12 +6,12 @@ const result = document.getElementById("result");
 let temp;
 
 function convert(){
-    if(toFahrenheit,checked){
+    if(toFahrenheit.checked){
         temp = Number(textBox.value);
         temp = temp * 9 / 5 + 32;
         result.textContent = temp.toFixed(1) + "°F";
     }
-    else if(toCelsius,checked){
+    else if(toCelsius.checked){
         temp = Number(textBox.value);
         temp = (temp - 32) * (5/9);
         result.textContent = temp.toFixed(1) + "°F";

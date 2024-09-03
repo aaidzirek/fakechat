279f2f50aeb1030f172c687adb2b72af946af6b3
aaid <zirekaaid@gmail.com>
2024-09-03 23:39:06 +0100

    dd

--- every_js/Closures.js
+++ every_js/Closures.js
@@ -5,6 +5,42 @@
 //                  Used frequently in JS frameworks: React, Vue, Angular
 
 // --------- EXAMPLE 1 ---------
-let myArray = ["A", "s", "d", "f", "g", "h"]
-myArray.sort()
-console.log(myArray);
+// function outer(){
+
+//     let message = "Hello";
+
+//     function inner(){
+//         console.log(message);
+//     }
+//     inner();
+// }
+// outer();
+
+// --------- EXAMPLE 2 ---------let count = 0;
+function createCounter(){
+    
+    let count = 0;
+
+    function increment(){
+        count++;
+        console.log(`count increased to ${count}`);
+    }
+
+    function getCount(){
+        return count;
+    }
+    return {increment, getCount}
+
+}
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(`The current count is ${counter.getCount()}`);
+
+
+// counter.count = 0;
+// console.log(counter.count);

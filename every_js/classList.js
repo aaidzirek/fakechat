11228133dc2002b54095cdb31713ea89d53eaaec
aaid <zirekaaid@gmail.com>
2024-10-05 09:35:35 +0100

    aaid darsa

--- every_js/classList.js
+++ every_js/classList.js
@@ -9,15 +9,17 @@
 // replace(oldclass, newClass);
 // contains();
 
+/*
+
 const myButton = document.getElementById("myButton")
 
-// myButton.addEventListener("mouseover", event => {
-//     event.target.classList.toggle("hover");
-// });
+myButton.addEventListener("mouseover", event => {
+    event.target.classList.toggle("hover");
+});
 
-// myButton.addEventListener("mouseout", event => {
-//     event.target.classList.toggle("hover");
-// });
+myButton.addEventListener("mouseout", event => {
+    event.target.classList.toggle("hover");
+});
 
 myH1.classList.add("enabled")
 myButton.classList.add("enabled");
@@ -31,7 +33,7 @@ myH1.addEventListener("click", event => {
         event.target.classList.replace("enabled", "disabled")
     }
 
-})
+});
 
 myButton.addEventListener("click", event => {
 
@@ -42,4 +44,34 @@ myButton.addEventListener("click", event => {
         event.target.classList.replace("enabled", "disabled")
     }
 
-})
+});  */
+
+let buttons = document.querySelectorAll(".myButtons");
+
+buttons.forEach(button => {
+    button.classList.add("enabled")
+});
+
+buttons.forEach(button => {
+    button.addEventListener("mouseover", event => {
+        event.target.classList.toggle("hover")
+    });
+});
+
+buttons.forEach(button => {
+    button.addEventListener("mouseout", event => {
+        event.target.classList.toggle("hover")
+    });
+});
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+
+        if(event.target.classList.contains("disabled")){
+            event.target.textContent += "💨"
+        }
+        else{
+            event.target.classList.replace("enabled", "disabled")
+        };
+    });
+});
